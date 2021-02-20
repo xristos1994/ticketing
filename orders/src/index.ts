58408197be4c20db0d27d0d112a075dfbad38356
xristos1994 <44114413+xristos1994@users.noreply.github.com>
2021-02-20 10:35:34 +0200

    Add startup message to orders service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,6 +7,7 @@ import { PaymentCreatedListener } from './events/listeners/payment-created-liste
 import { app } from './app';
 
 const start = async () => {
+  console.log('Orders starting...');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
